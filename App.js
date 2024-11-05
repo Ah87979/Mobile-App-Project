354d3bdacce061c4f2257e1e43d8dbd75169a902
Ahmed <ah87979@gmail.com>
2024-11-05 17:37:16 +0300

    Added Firebase connectivity

--- App.js
+++ App.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Home = () => {
+import Login from './pages/Login.js';
+import Home from './pages/Home.js';
+  
+const Details = ({navigation}) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-    )
-  }
-
-const Details = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Text>Details Screen</Text>
+      <Button title="Go to Games..." onPress={() => navigation.navigate('Games')} />
     </View>
-    )
-  }
-
+  )
+}
+  
 const Stack = createNativeStackNavigator()
-
+  
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name='Home' component={Home} options={{ title: 'Overview' }} />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name='Login' component={Login} options={{headerShown: false}} />
         <Stack.Screen name='Details' component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -36,11 +32,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
   },
 });
---
+++ pages/Login.js
@@ -0,0 +1,68 @@
+import { StyleSheet, Dimensions, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { auth, db } from '../config';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+const {width, height} = Dimensions.get('window');
+const myFontSize = (width+height) * 0.02;
+
+const Login = ({ navigation }) => {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+
+    const handleRegister = () => {
+       //auth: This is the Firebase authentication object, initialized earlier in the config.js
+       //.then(() => console.log("registered")): This is a promise-based then block that runs after the createUserWithEmailAndPassword operation is successful. If the user account is created successfully, it logs "registered" to the console.
+       //.catch((error) => console.log(error)): This is a catch block that runs if there is an error during the registration process, like if the email is already in use or the password is too weak.  
+       createUserWithEmailAndPassword(auth, email, password)
+        .then(() => console.log("registered"))
+        .catch((error) => console.log(error))
+        }
+        const handleLogin = () => {
+            signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+            console.log('Logged in')
+            navigation.replace('Home')
+            })
+            .catch((error) => {console.log(error.message);
+            setSignedIn(false)})
+            }
+    return (
+        <SafeAreaView style={styles.container}>
+            <TextInput placeholder='Email' style={styles.txtIn} onChangeText={(txt)=>setEmail(txt)}/>
+            <TextInput placeholder='Password' style={styles.txtIn} onChangeText={(txt)=>setPassword(txt)}/>
+            <TouchableOpacity style={styles.touch} onPress={handleLogin}>
+                <Text style={styles.txt}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.touch} onPress={handleRegister}>
+                <Text style={styles.txt}>Register</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightgrey',
+    },
+    txtIn:{
+        backgroundColor: 'snow',
+        padding: myFontSize * 0.5,
+        width: width * 0.85,
+        marginTop: height * 0.02,
+        borderRadius: 10,
+    },
+    touch:{
+        width: width/4,
+        backgroundColor: 'lightblue',
+        marginTop: height * 0.02,
+        padding: myFontSize * 0.5,
+        borderRadius: 10,
+    },
+   
+})
